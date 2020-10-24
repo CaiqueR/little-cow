3072e9789c1422f927725fd31f33106e15a5ba2f
lucas <lucas.laborne@gmail.com>
2020-10-24 11:21:51 -0300

    adiciona gitignore no backend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,10 @@
 import express from 'express';
 
 const app = express();
+const PORT = process.env.PORT || 3333;
 
 app.get('/', (req, res) => res.json({ message: 'Hellow mundo' }));
 
-app.listen(3333);
+app.listen(PORT, ()=>{
+    console.log(`Escutando na porta ${PORT}`);
+});
