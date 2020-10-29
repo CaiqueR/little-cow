23e4f9a75bc921b627a12d9fa1726f47468e8fae
Caique <caiiquexd@gmail.com>
2020-10-28 22:27:48 -0300

    styled components

---
+++ frontend/src/components/Page/Page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import { Container } from "./style";
+
+const Teste: React.FC = () => {
+  return <Container>Aplicação inicial</Container>;
+};
+
+export default Teste;
