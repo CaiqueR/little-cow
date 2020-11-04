bf053a3a1b51619e6273bdcae5e35837d76a1cc7
Caique Ribeiro de Oliveira <42003129+CaiqueR@users.noreply.github.com>
2020-11-04 09:30:45 -0300

    inclui titulos das paginas (#43)
    Co-authored-by: Caique <caiiquexd@gmail.com>

--- frontend/src/components/Content/index.tsx
+++ frontend/src/components/Content/index.tsx
@@ -1,10 +1,13 @@
 import React from "react";
-
+import { Helmet } from "react-helmet";
 import { Container } from "./styles";
 
 const Content: React.FC = () => {
   return (
     <Container>
+      <Helmet>
+        <title>Little Cow | Página Inicial</title>
+      </Helmet>
       <h1>Ajude alguém a financiar seu projeto</h1>
       <span>Alguem ficará muito feliz com sua ajuda</span>
 
