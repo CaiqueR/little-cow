20e3198d5d785c60334cd946b5edab093a613824
Lucas Laborne <34322384+lbltavares@users.noreply.github.com>
2020-11-28 09:07:40 -0300

    Update api.ts

--- frontend/src/service/api.ts
+++ frontend/src/service/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import useSWR from "swr";
 
 const api = axios.create({
-  baseURL: "http://m1.lbltavares.cloud:8080",
+  baseURL: "https://ubuntu-79bb94a9.localhost.run",
 });
 
 export default api;
